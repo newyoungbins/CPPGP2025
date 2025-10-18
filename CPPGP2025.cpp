0dc761409015618200a4d5705d2c674222235094
newyoungbins <dudqlsjyb@naver.com>
2025-10-18 10:11:57 +0900

    feat : graphics

--- CPPGP2025.cpp
+++ CPPGP2025.cpp
@@ -1,221 +1,221 @@
-﻿// CPPGP2025.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+﻿//// CPPGP2025.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+////
+//#include <Windows.h>
+//#include <iostream>
+//#include "GameState.h"
+//#include "ZVector3.h"
+//#include "ZMatrix.h"
+//#include "TitleState.h"
+//#include "CPPGP2025.h"
+//#include "Bitmap.h"
 //
-#include <Windows.h>
-#include <iostream>
-#include "GameState.h"
-#include "ZVector3.h"
-#include "ZMatrix.h"
-#include "TitleState.h"
-#include "CPPGP2025.h"
-#include "Bitmap.h"
-
-#define WIN_NAME L"WinAPI State Pattern";
-#define WIN_WIDTH 800
-#define WIN_HEIGHT 600
-
-GameState* g_currentState = nullptr;
-HWND g_hWnd;
-
-void MatrixTest();
-void ChangeState(GameState* newState);
-
-LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
-{
-    switch (message)
-    {
-    case WM_CREATE:
-        timeBeginPeriod(1);
-        break;
-
-    case WM_PAINT:
-        {
-        if (g_currentState)
-        {
-        PAINTSTRUCT ps;
-        HDC hdc = BeginPaint(hWnd, &ps);
-        //g_currentState->Render(wParam);                   //----------------------------------------<<<< 이거 에러나요
-        EndPaint(hWnd, &ps);
-        }
-        break;
-        }
-    case WM_DESTROY: // 창이 닫힐 때 발생
-        PostQuitMessage(0); // 메세지 루프를 종료하도록 요청
-        break;
-    case WM_KEYDOWN:
-        if (g_currentState)
-        {
-            g_currentState->OnKeyDown(wParam);
-        }
-        break;
-    case WM_KEYUP:
-        if (g_currentState)
-        {
-            g_currentState->OnkeyUp(wParam);
-        }
-        break;
-    case WM_LBUTTONDOWN:
-    case WM_RBUTTONDOWN:
-    case WM_MBUTTONDOWN:
-        if (g_currentState)
-        {
-            g_currentState->OnMouseDown(LOWORD(lParam), HIWORD(lParam), (int)wParam);
-        }
-    case WM_LBUTTONUP:
-    case WM_RBUTTONUP:
-    case WM_MBUTTONUP:
-        if (g_currentState)
-        {
-            g_currentState->OnMouseUp(LOWORD(lParam), HIWORD(lParam), (int)wParam);
-        }
-    case WM_MOUSEMOVE:
-        if (g_currentState)
-        {
-            g_currentState->OnMouseMove(LOWORD(lParam), HIWORD(lParam));
-        }
-    default:
-        return DefWindowProc(hWnd, message, wParam, lParam);
-
-    }
-}
-
-void SetupConsole()
-{
-    AllocConsole(); // 현재 프로세스에 새로운 콘솔 창을 할당
-    FILE* pConsole;
-    // stdout에 쓰는 모든 내용이 새로 할당된 콘솔 창에 출력
-    freopen_s(&pConsole, "CONOUT$", "w", stdout);
-}
-
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevlnstance, LPSTR lpCmdLine, int nCmdShow)
-{
-    SetupConsole();
-    MatrixTest();
-
-    // 윈도우 클래스 정의
-    WNDCLASS wc = {};
-    wc.lpfnWndProc = WndProc;
-    wc.hInstance = hInstance;
-    wc.lpszClassName = L"CPPGPClass";
-    
-
-    //  윈도우 클래스 등록
-    RegisterClass(&wc);
-
-    // 윈도우 생성
-    HWND hWnd = CreateWindowEx(
-        0,                      // 확장 윈도우 스타일
-        L"CPPGPClass",          // 윈도우 클래스 이름
-        L"CPPGP",               // 윈도우 제목
-        WS_OVERLAPPEDWINDOW,    // 윈도우 스타일
-
-        // 크기 및 위치
-        CW_USEDEFAULT, CW_USEDEFAULT, WIN_WIDTH, WIN_HEIGHT,
-
-        NULL, // 부모 윈도우
-        NULL, // 메뉴
-        hInstance,
-        NULL // 추가 애플리케이션 데이터
-    );
-
-    if (hWnd == NULL)
-    {
-        std::cout << "Failed to create window!" << std::endl;
-        return 0;
-    }
-
-    // 윈도우를 화면에 표시 및 업데이트
-    ShowWindow(hWnd, nCmdShow);
-    UpdateWindow(hWnd);
-
-    // 시작 상태
-    ChangeState(new TitleState());
-
-    long long prevTime = timeGetTime();
-
-    MSG msg = {};
-    while (GetMessage(&msg, NULL, 0, 0))
-    {
-        if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
-        {
-        TranslateMessage(&msg);
-        DispatchMessage(&msg);
-        }
-        else
-        {
-            long long currentTime = timeGetTime();
-            float elapsedTime = (currentTime - prevTime) / 1000.0f;
-            prevTime = currentTime;
-
-            if (g_currentState)
-            {
-                g_currentState->Update(elapsedTime);
-            }
-
-            InvalidateRect(g_hWnd, NULL, FALSE);
-        }
-    }
-
-    if (g_currentState)
-    {
-        g_currentState->Exit();
-        delete g_currentState;
-        g_currentState = nullptr;
-    }
-
-    FreeConsole();
-    return (int)msg.wParam;
-}
-
-void MatrixTest()
-{
-    ZVector3 v1(1, 2, 3);
-    std::cout << v1 << std::endl;
-
-    ZVector3 v2(4, -5, 6);
-    std::cout << v2.Cross(v1) << std::endl;
-
-    std::cout << ZVector3::Dot(v1, v2) << std::endl;
-    std::cout << v1.radBetween(v1, v2) << std::endl;
-    std::cout << v1.degBetween(v1, v2) << std::endl;
-
-    // Matrix 테스트
-    ZVector3 localPoint(1.0, 1.0, 1.0);
-    std::cout << "Local Space Point : " << localPoint << std::endl;
-
-    // Scale 모든 축으로 2배 확대
-    ZMatrix matScale = ZMatrix::CreateScale(2.0, 2.0, 2.0);
-
-    // Rotation : Y축 90도 회전
-    const double PI = 3.1415926535897932;
-    ZMatrix matRotation = ZMatrix::CreateRotationY(PI / 2.0);
-
-    // Translation : 월드 공간의 (5, 6, 7) 위치로 이동
-    ZMatrix matTranslation = ZMatrix::CreateTranslation(5.0, 6.0, 7.0);
-
-    // 변환을 순차적으로 적용
-    ZVector3 pointAfterScale = localPoint.Transform(matScale);
-    std::cout << pointAfterScale << std::endl;
-
-    ZVector3 pointAfterRotation = pointAfterScale.Transform(matRotation);
-    std::cout << pointAfterRotation << std::endl;
-
-    ZVector3 finalWorldPoint = pointAfterRotation.Transform(matTranslation);
-    std::cout << finalWorldPoint << std::endl;
-
-    // 한방에 월드 매트릭스
-    ZMatrix worldMatrix = matScale * matRotation * matTranslation;
-    ZVector3 finalWorldPointByMatrix = localPoint.Transform(worldMatrix);
-    std::cout << finalWorldPointByMatrix << std::endl;
-}
-
-void ChangeState(GameState* newState)
-{
-    if (g_currentState)
-    {
-        g_currentState->Exit();
-        delete g_currentState;
-
-    }
-    g_currentState = newState;
-    g_currentState->Enter(g_hWnd);
-}
+//#define WIN_NAME L"WinAPI State Pattern";
+//#define WIN_WIDTH 800
+//#define WIN_HEIGHT 600
+//
+//GameState* g_currentState = nullptr;
+//HWND g_hWnd;
+//
+//void MatrixTest();
+//void ChangeState(GameState* newState);
+//
+//LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+//{
+//    switch (message)
+//    {
+//    case WM_CREATE:
+//        timeBeginPeriod(1);
+//        break;
+//
+//    case WM_PAINT:
+//        {
+//        if (g_currentState)
+//        {
+//        PAINTSTRUCT ps;
+//        HDC hdc = BeginPaint(hWnd, &ps);
+//        //g_currentState->Render(wParam);                   //----------------------------------------<<<< 이거 에러나요
+//        EndPaint(hWnd, &ps);
+//        }
+//        break;
+//        }
+//    case WM_DESTROY: // 창이 닫힐 때 발생
+//        PostQuitMessage(0); // 메세지 루프를 종료하도록 요청
+//        break;
+//    case WM_KEYDOWN:
+//        if (g_currentState)
+//        {
+//            g_currentState->OnKeyDown(wParam);
+//        }
+//        break;
+//    case WM_KEYUP:
+//        if (g_currentState)
+//        {
+//            g_currentState->OnkeyUp(wParam);
+//        }
+//        break;
+//    case WM_LBUTTONDOWN:
+//    case WM_RBUTTONDOWN:
+//    case WM_MBUTTONDOWN:
+//        if (g_currentState)
+//        {
+//            g_currentState->OnMouseDown(LOWORD(lParam), HIWORD(lParam), (int)wParam);
+//        }
+//    case WM_LBUTTONUP:
+//    case WM_RBUTTONUP:
+//    case WM_MBUTTONUP:
+//        if (g_currentState)
+//        {
+//            g_currentState->OnMouseUp(LOWORD(lParam), HIWORD(lParam), (int)wParam);
+//        }
+//    case WM_MOUSEMOVE:
+//        if (g_currentState)
+//        {
+//            g_currentState->OnMouseMove(LOWORD(lParam), HIWORD(lParam));
+//        }
+//    default:
+//        return DefWindowProc(hWnd, message, wParam, lParam);
+//
+//    }
+//}
+//
+//void SetupConsole()
+//{
+//    AllocConsole(); // 현재 프로세스에 새로운 콘솔 창을 할당
+//    FILE* pConsole;
+//    // stdout에 쓰는 모든 내용이 새로 할당된 콘솔 창에 출력
+//    freopen_s(&pConsole, "CONOUT$", "w", stdout);
+//}
+//
+//int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevlnstance, LPSTR lpCmdLine, int nCmdShow)
+//{
+//    SetupConsole();
+//    MatrixTest();
+//
+//    // 윈도우 클래스 정의
+//    WNDCLASS wc = {};
+//    wc.lpfnWndProc = WndProc;
+//    wc.hInstance = hInstance;
+//    wc.lpszClassName = L"CPPGPClass";
+//    
+//
+//    //  윈도우 클래스 등록
+//    RegisterClass(&wc);
+//
+//    // 윈도우 생성
+//    HWND hWnd = CreateWindowEx(
+//        0,                      // 확장 윈도우 스타일
+//        L"CPPGPClass",          // 윈도우 클래스 이름
+//        L"CPPGP",               // 윈도우 제목
+//        WS_OVERLAPPEDWINDOW,    // 윈도우 스타일
+//
+//        // 크기 및 위치
+//        CW_USEDEFAULT, CW_USEDEFAULT, WIN_WIDTH, WIN_HEIGHT,
+//
+//        NULL, // 부모 윈도우
+//        NULL, // 메뉴
+//        hInstance,
+//        NULL // 추가 애플리케이션 데이터
+//    );
+//
+//    if (hWnd == NULL)
+//    {
+//        std::cout << "Failed to create window!" << std::endl;
+//        return 0;
+//    }
+//
+//    // 윈도우를 화면에 표시 및 업데이트
+//    ShowWindow(hWnd, nCmdShow);
+//    UpdateWindow(hWnd);
+//
+//    // 시작 상태
+//    ChangeState(new TitleState());
+//
+//    long long prevTime = timeGetTime();
+//
+//    MSG msg = {};
+//    while (GetMessage(&msg, NULL, 0, 0))
+//    {
+//        if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+//        {
+//        TranslateMessage(&msg);
+//        DispatchMessage(&msg);
+//        }
+//        else
+//        {
+//            long long currentTime = timeGetTime();
+//            float elapsedTime = (currentTime - prevTime) / 1000.0f;
+//            prevTime = currentTime;
+//
+//            if (g_currentState)
+//            {
+//                g_currentState->Update(elapsedTime);
+//            }
+//
+//            InvalidateRect(g_hWnd, NULL, FALSE);
+//        }
+//    }
+//
+//    if (g_currentState)
+//    {
+//        g_currentState->Exit();
+//        delete g_currentState;
+//        g_currentState = nullptr;
+//    }
+//
+//    FreeConsole();
+//    return (int)msg.wParam;
+//}
+//
+//void MatrixTest()
+//{
+//    ZVector3 v1(1, 2, 3);
+//    std::cout << v1 << std::endl;
+//
+//    ZVector3 v2(4, -5, 6);
+//    std::cout << v2.Cross(v1) << std::endl;
+//
+//    std::cout << ZVector3::Dot(v1, v2) << std::endl;
+//    std::cout << v1.radBetween(v1, v2) << std::endl;
+//    std::cout << v1.degBetween(v1, v2) << std::endl;
+//
+//    // Matrix 테스트
+//    ZVector3 localPoint(1.0, 1.0, 1.0);
+//    std::cout << "Local Space Point : " << localPoint << std::endl;
+//
+//    // Scale 모든 축으로 2배 확대
+//    ZMatrix matScale = ZMatrix::CreateScale(2.0, 2.0, 2.0);
+//
+//    // Rotation : Y축 90도 회전
+//    const double PI = 3.1415926535897932;
+//    ZMatrix matRotation = ZMatrix::CreateRotationY(PI / 2.0);
+//
+//    // Translation : 월드 공간의 (5, 6, 7) 위치로 이동
+//    ZMatrix matTranslation = ZMatrix::CreateTranslation(5.0, 6.0, 7.0);
+//
+//    // 변환을 순차적으로 적용
+//    ZVector3 pointAfterScale = localPoint.Transform(matScale);
+//    std::cout << pointAfterScale << std::endl;
+//
+//    ZVector3 pointAfterRotation = pointAfterScale.Transform(matRotation);
+//    std::cout << pointAfterRotation << std::endl;
+//
+//    ZVector3 finalWorldPoint = pointAfterRotation.Transform(matTranslation);
+//    std::cout << finalWorldPoint << std::endl;
+//
+//    // 한방에 월드 매트릭스
+//    ZMatrix worldMatrix = matScale * matRotation * matTranslation;
+//    ZVector3 finalWorldPointByMatrix = localPoint.Transform(worldMatrix);
+//    std::cout << finalWorldPointByMatrix << std::endl;
+//}
+//
+//void ChangeState(GameState* newState)
+//{
+//    if (g_currentState)
+//    {
+//        g_currentState->Exit();
+//        delete g_currentState;
+//
+//    }
+//    g_currentState = newState;
+//    g_currentState->Enter(g_hWnd);
+//}
--- GameMain.cpp
+++ GameMain.cpp
@@ -2,16 +2,49 @@
 #include <mmsystem.h> // timeGetTime()
 #pragma comment(lib, "winmm.lib")
 
+void SetupConsole()
+{
+	AllocConsole();
+	FILE* pConsole;
+	freopen_s(&pConsole, "CONOUT$", "w", stdout);
+}
+
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
-	ZApp Game(_T("My D3D Test"), 0, 0, 800, 600);
-	return Game.Run();
+	SetupConsole();
+
+	// 클라이언트 영역 크기
+	const int clientWidth = 1024;
+	const int clientHeight = 600;
+
+	RECT windowRect = { 0, 0, clientWidth, clientHeight };
+	AdjustWindowRect(&windowRect, WS_OVERLAPPEDWINDOW, FALSE);
+
+	int windowWidth = windowRect.right - windowRect.left;
+	int windowHeight = windowRect.bottom - windowRect.top;
+
+	std::cout << "windowRect: " << windowWidth << " " << windowHeight << std::endl;
+
+	// 화면 중앙 위치 계산
+	int screenWidth = GetSystemMetrics(SM_CXSCREEN);
+	int screenHeight = GetSystemMetrics(SM_CYSCREEN);
+
+	int x = (screenWidth - windowWidth) / 2;
+	int y = (screenHeight - windowHeight) / 2;
+
+	ZApp Game(_T("My D3D Test"), x, y, windowWidth, windowHeight, clientWidth, clientHeight);
+	BOOL result = Game.Run();
+
+	FreeConsole();
+
+	return result;
 }
 
 ZApp::ZApp(const TCHAR* pszCaption,
-	DWORD XPos, DWORD YPos, DWORD Width, DWORD Height)
-	: ZApplication(XPos, YPos, Width, Height)
+	DWORD XPos, DWORD YPos, DWORD Width, DWORD Height, DWORD ClientWidth, DWORD ClientHeight)
+	: ZApplication(XPos, YPos, Width, Height, ClientWidth, ClientHeight)
 {
+	m_pGraphics = nullptr;
 	_tcscpy_s(m_Caption, pszCaption);
 }
 
@@ -42,7 +75,9 @@ BOOL ZApp::Shutdown()
 
 BOOL ZApp::Init()
 {
-	m_pGraphics = new ZGraphics(GetHWnd());
+	m_pGraphics = new ZGraphics(GetHWnd(), 
+		(float)m_ClientHeight / (float)m_ClientWidth, 
+		m_ClientWidth, m_ClientHeight);
 
 	ShowMouse(TRUE);
 
@@ -51,7 +86,33 @@ BOOL ZApp::Init()
 
 BOOL ZApp::Frame()
 {
-	m_pGraphics->ClearBuffer(0, 0, 1.0f);
+	// timeGetTime() 함수는 시스템이 시작된 후 경과된 시간을 밀리초(ms) 단위로 반환합니다.
+	DWORD timeValue = timeGetTime();
+	// 경과 시간을 초 단위로 변환합니다. (1000ms = 1s)
+	double dValue = timeValue / 1000.0;
+	// sin 함수를 사용하여 시간의 흐름에 따라 0.0 ~ 1.0 사이를 부드럽게 왕복하는 값을 계산합니다.
+	// sin(dValue)의 결과는 -1.0 ~ 1.0 이므로, 이를 0.0 ~ 1.0 범위로 정규화합니다.
+	const float c = (float)sin(dValue) / 2.0f + 0.5f;
+	// 계산된 'c' 값을 사용하여 화면 배경색을 동적으로 변경합니다.
+	// Red와 Green 채널이 'c' 값에 따라 변하므로, 배경색이 파란색(0,0,1)과 청록색(1,1,1) 사이를 오가게 됩니다.
+	m_pGraphics->ClearBuffer(c, c, 1.0f);
+
+	//m_pGraphics->DrawTriangle();
+	//m_pGraphics->DrawIndexedTriangle();
+	//m_pGraphics->DrawConstTriangle((float)dValue);
+
+
+	// 윈도우 내에서 현재 마우스 위치
+	RECT rect;
+	GetWindowRect(GetHWnd(), &rect);
+	POINT pt;
+	GetCursorPos(&pt);
+	pt.x -= rect.left;
+	pt.y -= rect.top;
+	std::cout << pt.x << " " << pt.y << std::endl;
+	m_pGraphics->DrawDepthCube((float)dValue, pt.x, pt.y);
+
+	// 렌더링된 후면 버퍼를 화면에 표시합니다.
 	m_pGraphics->EndFrame();
 	return TRUE;
 }
