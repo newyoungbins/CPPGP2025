c0b31f15e17109ad8d949e688597d0437baba69d
newyoungbins <dudqlsjyb@naver.com>
2025-10-18 13:03:56 +0900

    feat : GetInfoManager

--- ZGraphicsResource.h
+++ ZGraphicsResource.h
@@ -6,7 +6,7 @@ class DxgiInfoManager;
 
 class ZGraphicsResource
 {
-protected:
+protected: // ZGraphics friend 立辟 力距 格利
     static ID3D11DeviceContext* GetContext(ZGraphics& gfx) noexcept;
     static ID3D11Device* GetDevice(ZGraphics& gfx) noexcept;
     static DxgiInfoManager& GetInfoManager(ZGraphics& gfx);
