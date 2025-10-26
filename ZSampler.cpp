96711d3205b4494bbb9e7ef27864bb6c66febefb
newyoungbins <dudqlsjyb@naver.com>
2025-10-26 11:42:16 +0900

    feat : ZSample

---
+++ ZSampler.cpp
@@ -0,0 +1,24 @@
+#include "ZD3D11.h"
+#include "ZSampler.h"
+#include "GraphicsThrowMacros.h"
+
+namespace Bind
+{
+	ZSampler::ZSampler(ZGraphics& gfx)
+	{
+		INFOMAN(gfx);
+
+		D3D11_SAMPLER_DESC samplerDesc = {};
+		samplerDesc.Filter = D3D11_FILTER_MIN_MAG_MIP_POINT;
+		samplerDesc.AddressU = D3D11_TEXTURE_ADDRESS_WRAP;
+		samplerDesc.AddressV = D3D11_TEXTURE_ADDRESS_WRAP;
+		samplerDesc.AddressW = D3D11_TEXTURE_ADDRESS_WRAP;
+
+		GFX_THROW_INFO(GetDevice(gfx)->CreateSamplerState(&samplerDesc, &pSampler));
+	}
+
+	void ZSampler::Bind(ZGraphics& gfx) noexcept
+	{
+		GetContext(gfx)->PSSetSamplers(0, 1, pSampler.GetAddressOf());
+	}
+}
